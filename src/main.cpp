9973b83fa4ef6311fef0bbf4af4184c0fc0efe70
Judson Hartley <judson.hartley2@gmail.com>
2022-01-13 08:59:11 -0500

    update

--- src/main.cpp
+++ src/main.cpp
@@ -34,14 +34,28 @@
 
 #include <Arduino.h>            //Add arduino functions
 #include <FlexyStepper.h>       //Add Stepper Library
+#include <ButtonKing.h>         //Add button king debounce
+
+//buttons
+ButtonKing trigger(TriggerPin, true, true);
+ButtonKing reload(ReloadPin, true, true);
+ButtonKing index(IndexPin, true, true);
+ButtonKing animation(AnimationPin, true, true);
+#ifdef DebugStepper
+  ButtonKing debugMeasure(DebugMeasureStepsBetweenIndexPin, true,true);
+#endif
 
 
 FlexyStepper stepper;           //Create stepper object
 
+//variables
+bool stopMotion = false;
 
+void buttonCheck();
 void IndexRoutine();
 void FireRoutine();
 void AnimationRoutine();
+void StopMotion();
 #ifdef DebugStepper
   void MeasureStepsBetweenIndexPin();
 #endif
@@ -54,14 +68,21 @@ void setup() {  // put your setup code here, to run once:
   #endif
   DebugSerial.println("SPNKR Launcher starting");
   DebugSerial.print("Setup input pins...");
-  pinMode(TriggerPin,INPUT_PULLUP);
+/*  pinMode(TriggerPin,INPUT_PULLUP);
   pinMode(ReloadPin, INPUT_PULLUP);
-  pinMode(IndexPin, INPUT_PULLUP);
   pinMode(AnimationPin, INPUT_PULLUP);
   #ifdef DebugStepper
     pinMode(DebugStepPin, INPUT_PULLUP);
     pinMode(DebugDirectionPin, INPUT_PULLUP);
     pinMode(DebugMeasureStepsBetweenIndexPin, INPUT_PULLUP);
+  #endif */
+  pinMode(IndexPin, INPUT_PULLUP);
+  trigger.setRelease(FireRoutine);
+  animation.setRelease(AnimationRoutine);
+  reload.setPress(IndexRoutine);
+  reload.setRelease();
+  #ifdef DebugStepper
+    debugMeasure.setRelease(MeasureStepsBetweenIndexPin);
   #endif
   DebugSerial.println(" complete.");
 
@@ -71,19 +92,35 @@ void setup() {  // put your setup code here, to run once:
 }
 
 void loop() {  // put your main code here, to run repeatedly:
+  buttonCheck();
+}
+
+void buttonCheck(){
   //code for button inputs to call routines...
+  trigger.isClick();
+  reload.isClick();
+  //index.isClick();
+  animation.isClick();
+  #ifdef DebugStepper
+    debugMeasure.isClick();
+  #endif
 }
 
 void IndexRoutine(){ //routine to index drum
-
+  stopMotion = false; //allow motion
 }
 
 void FireRoutine(){ //routine to switch barrels after firing
-
+  if (stopMotion) {return;} //if motion not allowed do nothing
 }
 
 void AnimationRoutine(){ //routine to perform animation
-  
+  if (stopMotion) {return;} //if motion not allowed do nothing
+}
+
+void StopMotion(){
+  stopMotion = true; //Stop motion...
+  stepper.setTargetPositionToStop(); //stop motion...
 }
 
 #ifdef DebugStepper
